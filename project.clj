36910735b1b7171bf6ba21f269d8fa48548b9774
Karlis Lauva <k@karlis.me>
2016-07-01 10:54:48 -0600

    Version 0.10.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/full.bootstrap "0.10.2-SNAPSHOT"
+(defproject fullcontact/full.bootstrap "0.10.2"
   :description "Boostrap module that pulls in all commonly used full-monty dependencies."
   :url "https://github.com/fullcontact/full.bootstrap"
   :license {:name "Eclipse Public License - v 1.0"
