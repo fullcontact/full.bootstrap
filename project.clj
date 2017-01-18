f74e5a3f902948bacb5d5b55e92bed6f5dbf4474
zarlen <zach@fullcontact.com>
2017-01-18 15:56:34 -0700

    Version 0.10.5-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/full.bootstrap "0.10.4"
+(defproject fullcontact/full.bootstrap "0.10.5-SNAPSHOT"
   :description "Boostrap module that pulls in all commonly used full-monty dependencies."
   :url "https://github.com/fullcontact/full.bootstrap"
   :license {:name "Eclipse Public License - v 1.0"
